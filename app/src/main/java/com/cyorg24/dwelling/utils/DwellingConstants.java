23b59c8637a2753257aa09d9e083ac4f98d6c37f
yoga24 <yorganand@gmail.com>
2021-08-08 15:49:37 +0530

    Updates for National Insurance

--- app/src/main/java/com/cyorg24/dwelling/utils/DwellingConstants.java
+++ app/src/main/java/com/cyorg24/dwelling/utils/DwellingConstants.java
@@ -5,7 +5,7 @@ public class DwellingConstants {
     public static final String YEARS_INSURED_KEY = "years";
     public static final String PREMIUM_OBJECT_KEY = "premiumObjectModel";
 
-    public static final double BASIC_PREMIUM_RATE = 0.181 / 1000;
+    public static final double BASIC_PREMIUM_RATE = 0.155 / 1000;
     public static final double TERRORISM = 0.08 / 1000;
     public static final double SERVICE_TAX = 18.0 / 100;
 
