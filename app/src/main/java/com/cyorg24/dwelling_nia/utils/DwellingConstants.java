33e620bf925a94506836ae855d8db0abf6ddc036
yoga24 <yorganand@gmail.com>
2025-03-17 00:29:49 +0530

    flavors

--- app/src/main/java/com/cyorg24/dwelling_nia/utils/DwellingConstants.java
+++ app/src/main/java/com/cyorg24/dwelling_nia/utils/DwellingConstants.java
@@ -1,11 +1,13 @@
 package com.cyorg24.dwelling_nia.utils;
 
+import com.cyorg24.dwelling_nia.BuildConfig;
+
 public class DwellingConstants {
 
     public static final String YEARS_INSURED_KEY = "years";
     public static final String PREMIUM_OBJECT_KEY = "premiumObjectModel";
 
-    public static final double BASIC_PREMIUM_RATE = 0.13 / 1000;
+    public static final double BASIC_PREMIUM_RATE = BuildConfig.BASIC_PREMIUM / 1000;
     public static final double SERVICE_TAX = 18.0 / 100;
 
 }
